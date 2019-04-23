800f90ad462565caffa55f274ccd75a957f65607
向昌鑫 <42061526+LittleFishUP@users.noreply.github.com>
2019-04-23 11:04:36 +0800

    add base
    this is the Framebase

---
+++ performanceprocess/src/main/java/com/dxc/activitidemo/performanceprocess/PerformanceprocessApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.activitidemo.performanceprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceprocessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceprocessApplication.class, args);
+    }
+
+}
---
+++ performanceprocess/src/test/java/com/dxc/activitidemo/performanceprocess/PerformanceprocessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dxc.activitidemo.performanceprocess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerformanceprocessApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
