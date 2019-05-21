6edfc4b651901f75fc48a39a4be9120d9ca75ee8
向昌鑫 <42061526+LittleFishUP@users.noreply.github.com>
2019-05-21 10:15:53 +0800

    updata

---
+++ performanceprocess/src/main/java/com/dxc/activitidemo/performanceprocess/controller/activitiController.java
@@ -0,0 +1,9 @@
+package com.dxc.activitidemo.performanceprocess.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/goal")
+public class activitiController {
+}
--- performanceprocess/src/main/java/com/dxc/activitidemo/performanceprocess/controller/mangementController.java
+++ performanceprocess/src/main/java/com/dxc/activitidemo/performanceprocess/controller/mangementController.java
@@ -2,11 +2,16 @@ package com.dxc.activitidemo.performanceprocess.controller;
 
 import com.dxc.activitidemo.performanceprocess.dao.*;
 import com.dxc.activitidemo.performanceprocess.entity.*;
+import org.apache.poi.hssf.usermodel.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -188,6 +193,88 @@ public class mangementController {
 
         return "CheckAllGoal";
     }
+
+
+    @RequestMapping("/downloadExcel")
+    public void setUpExcel(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String[] headers = { "UserID", "name","Group","Manager","Title","Describe","status","selfEval","MREval","MRGrade"};
+        List<CheckAllGoal> CheckAllGoalenittylist = goalMapper.selectallGoal();
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet();
+        //设置列宽
+        sheet.setDefaultColumnWidth((short) 18);
+        //创建第一行的对象，第一行一般用于填充标题内容。从第二行开始一般是数据
+        HSSFRow row = sheet.createRow(0);
+        for (short i = 0; i < headers.length; i++) {
+            //创建单元格，每行多少数据就创建多少个单元格
+            HSSFCell cell = row.createCell(i);
+            HSSFRichTextString text = new HSSFRichTextString(headers[i]);
+            //给单元格设置内容
+            cell.setCellValue(text);
+        }
+
+        //遍历集合，将每个集合元素对象的每个值填充到单元格中
+        for(int i=0;i<CheckAllGoalenittylist.size();i++){
+            CheckAllGoal checkAllGoal = CheckAllGoalenittylist.get(i);
+            //从第二行开始填充数据
+            row = sheet.createRow(i+1);
+            List<String> datas=new ArrayList<>();
+
+            String UserID = checkAllGoal.getId().toString();
+            String name = checkAllGoal.getName();
+            String Group = checkAllGoal.getGroup();
+            String Manager;
+            if(checkAllGoal.getManagerId()==null){
+                Manager = "";
+            }else{
+                User user = userMapper.selectByPrimaryKey(checkAllGoal.getManagerId());
+                Manager = user.getName();
+            }
+            String Title = checkAllGoal.getTitle();
+            String descript = checkAllGoal.getDescript();
+            String status = checkAllGoal.getStatus();
+            String selfEval = checkAllGoal.getSelfEval();
+            String mrEval = checkAllGoal.getMREval();
+            String mrGrade = checkAllGoal.getMRGrade();
+
+
+            datas.add(UserID);
+            datas.add(name);
+            datas.add(Group);
+            datas.add(Manager);
+            datas.add(Title);
+            datas.add(descript);
+            datas.add(status);
+            datas.add(selfEval);
+            datas.add(mrEval);
+            datas.add(mrGrade);
+
+
+            for (int j=0;j<datas.size();j++) {
+                String string=datas.get(j);
+                HSSFCell cell = row.createCell(j);
+                HSSFRichTextString richString = new HSSFRichTextString(string);
+                HSSFFont font3 = workbook.createFont();
+                richString.applyFont(font3);
+                cell.setCellValue(richString);
+            }
+
+        }
+
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-disposition", "attachment;filename="+"AllGoal.xls");//Excel文件名
+        try {
+            response.flushBuffer();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //将workbook中的内容写入输出流中
+        workbook.write(response.getOutputStream());
+
+    }
+
 
 
 }
